f80ac1141e2fdd38986f66448b116477bbd6ce77
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-08-31 11:46:05 +0200

    helberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - prints Helberton on the screen
+ *
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+        char *h = "Holberton";
+
+	while(*h)
+	{
+	  putchar(h);
+	  h++;
+
+	}
+
+	putchar("\n);
+
+	return (0);
+}	
+	
+	    
