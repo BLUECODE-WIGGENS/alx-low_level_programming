83e60fa54110d52c6a929171d8f7e7645a3c5041
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-08-31 19:42:35 +0200

    checking

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,13 @@
-#include "main.h"
-/**
- *islower - check for lowercase character .
- * 
- *Return: 0 
- */
- 
-int _islower(int c)
-       {    
-       if(c >= 'a' && c <= 'z')
+#include "holberton.h"
 
-       { 
-       return 1;
-       }
-        else
-       return 0
-       }
+/**
+ * _islower - a function that checks for lowercase character
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
