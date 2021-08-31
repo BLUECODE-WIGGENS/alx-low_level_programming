5b7299a3e46c50a1c372bb2b223deda58e120be4
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-08-31 19:22:45 +0200

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- *print_alphabet_x10 - printing alphabet 10 times.
- * 
+ *print_alphabet_x10 - printing alphabet 10 times
  *Return: Always 0 (success)
- */ 
+ */
 void print_alphabet_x10(void)
 {
 char c;
 char i;
 
-for(i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
-for(c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
-} 
+}
 }
