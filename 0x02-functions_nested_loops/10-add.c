0286de5583a5a0ab029b179a25e48cec8b62073f
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-08-31 21:30:00 +0200

    times tables

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,12 @@
 
 /**
  *int add(int, int) - add two numbers
- *
+ *@x: first number to add
+ *@y: second number to add
  *Return: returns the added value
  */
 
 int add(int x, int y)
 {
-           return (x +y);
+       return (x +y);
 }
