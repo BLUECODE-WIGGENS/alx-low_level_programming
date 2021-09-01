7488bd1ee10f2bba8dc41f2be68a64c6592f3fd5
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-01 12:16:15 +0200

    fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void)
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(void)
 {
-        char alx[] = "Holberton"
-        int y = 0;
-         
-        while (alx[y] != '\0')
-        {
-                 _putchar(alx[y]);
-                 y++;
-        }
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-
 	return (0);
 }
