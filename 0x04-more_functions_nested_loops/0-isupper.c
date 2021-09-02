539014320cead63b1910a0f267d4b34bc38fce75
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-02 13:41:04 +0200

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ *@c: input letter
+ * Return: 1 if int c is uppercase, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
