a483d05f431220dedc5b54f15715759d8e2d8883
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-02 19:35:05 +0200

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include <main.h>
 
 /**
- * print_numbers - Prints numbers between 0 to 9.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
+	int y;
 
-	for (int i = 0; i <= 9; i++)
+	for (y = 48; y < 58; y++)
 	{
-		_putchar(i);
+		_putchar(y);
 	}
 	_putchar('\n');
 	return (0);
