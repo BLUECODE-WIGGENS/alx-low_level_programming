d56eaadd68b430471fb55b3a651f96bfbd791cb6
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-02 19:05:33 +0200

    multplication

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int mul(int a, int b);
 int _putchar(char c);
 int _isupper(int c);
 
---
+++ 0x04-more_functions_nested_loops/mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*mul - multiplication operation
+*@x: first number
+*@y: second number
+*Return: mul
+*/
+
+int mul(int x, int y)
+{
+int mul;
+
+mul = 0;
+mul = x * y;
+return (mul);
+}
