6774b4b72bd1be3141a3c9b37f0cc15945a1f13f
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-03 08:43:38 +0200

    fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,42 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *print_number - display digits
- *@n: integer
- *Return: nothing
- */
-
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
 void print_number(int n)
 {
+	unsigned int k = n;
 
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
 
-if (n >= 1000)
-{
-_putchar((n / 1000) + '0');
-_putchar(((n % 1000) / 100) + '0');
-_putchar(((n % 100) / 10) + '0');
-_putchar((n % 10) + '0');
-}
+	k /= 10;
 
-if ((n >= 100) && (n < 1000))
-{
-_putchar((n / 100) + '0');
-_putchar(((n % 100) / 10) + '0');
-_putchar((n % 10) + '0');
-}
+	if (k != 0)
+		print_number(k);
 
-if ((n >= 10) && (n < 100))
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-}
-if (n < 9 && n >= 0)
-{
-_putchar(n % 10 + '0');
-}
+	_putchar((unsigned int) n % 10 + '0');
+
 }
