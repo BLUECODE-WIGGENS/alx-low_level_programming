1e839980ecf71681403c353a94dd14e5cc7debdc
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-06 13:14:26 +0200

    reset 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *void reset_to_98  -  reset to 98.
- * @n :  the pointer
- */
+ *reset_to_98 - reset to 98.
+ * @n:  the pointer
+ **/
 void reset_to_98(int *n)
 {
-        *n = 98;
-        _putchar('\n');   
+        *n = 98;  
 }
