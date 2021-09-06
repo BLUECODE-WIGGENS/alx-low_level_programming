7b1dd21a1732bb9daf9e5a4030cdd02ed3ec49c8
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-06 14:38:08 +0200

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * strlen - returns the length of the string.
+ * _strlen - returns the length of the string.
  * @s: string
  * Return: int
  */
   int _strlen(char *s)
   {
-      int ch = 0;
-      while (*s != '\0')
-      {
-          ch++;
-          s++;
-      }
-      return ch;
+  int ch = 0;
+  while (*s != '\0')
+  {
+  ch++;
+  s++;
+  }
+  return ch;
   }
