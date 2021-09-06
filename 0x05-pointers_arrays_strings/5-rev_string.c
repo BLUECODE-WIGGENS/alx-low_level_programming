fd3c7c9eb9ee2bb506f730b9b813600f3e29c302
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-06 19:59:59 +0200

    strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* rev_string - reverse a string
+* @s: string
+**/
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char tmp;
+	int pos;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	pos = i - 1;
+	for (pos = i - 1; pos >= i / 2; pos--)
+	{
+		tmp = s[pos];
+		s[pos] = s[j];
+		s[j] = tmp;
+		j++;
+	}
+}
