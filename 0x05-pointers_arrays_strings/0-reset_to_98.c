191f3c5ea0a0829cb34a5676bbf483abc375665c
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-06 13:06:12 +0200

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main -  takes a pointer to an int as parameter and updates the value it points to to 98.
+ * main -  reset to 98.
  * @n :  the pointer
  */
-
 void reset_to_98(int *n)
 {
-                 *n = 98;   
+        *n = 98;   
 }
