76019fccd01395a3eff1b14d623c194b7a8f853d
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-15 19:22:20 +0200

    recursive minded

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,14 @@
 
 /**
  *_strlen_recursion - returns the length of a string
- * 
+ *
  * @s: a string to check the length
  */
 int _strlen_recursion(char *s)
 {
-if(*s == '\0')
+if (*s == '\0')
 {
-return (0);    
+return (0);
 }
-return 1 + _strlen_recursion(s + 1);    
+return (1 + _strlen_recursion(s + 1));
 }
