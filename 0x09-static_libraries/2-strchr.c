0b5fb69bdf438bb8e32f40dedb9bb89055f02ff0
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-19 08:18:39 +0200

    static

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string.
- * @s:
- * @c:
- * Return: c in the string s or NULL.\
+ *_strchr - locates character in a string
+ *@s: string
+ *@c: character
+ *
+ *Return: 0
  */
 
 char *_strchr(char *s, char c)
 {
-        
-	    int i;
 
-	    for (i = 0; *(s + i); i++)
-	    {
-		if (*(s + i) == c)
-            {
-			return (s + i);
-            }    
-	    }
-	    if (*(s + i) == c)
-        {
-		        return (s + i);
-        }       
-	return (0);
+do {
+if (*s == c)
+return ((char *)s);
+}
+while (*s++)
+;
+
+return (0);
 }
