46d5041a8802b1a06b92a2aa47bae406764cdadd
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-24 10:27:26 +0200

    fix it

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int a, b;
-	int c = n;
-	char *p;
-	int i, j;
+int a, b;
+int c = n;
+char *p;
+int i, j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
 
-	if (c >= j)
-	{
-		c = j;
-		p = malloc(sizeof(char) * (i + j + 1));
-	}
-	else
-		p = malloc(sizeof(char) * (i + n + 1));
-	if (p == NULL)
-		return (NULL);
-	for (a = 0; a < i; a++)
-	{
-		p[a] = s1[a];
-	}
-	for (b = 0; b < c; b++)
-	{
-		p[a++] = s2[b];
-	}
-	p[a++] = '\0';
-	return (p);
+if (c >= j)
+{
+c = j;
+p = malloc(sizeof(char) * (i + j + 1));
+}
+else
+p = malloc(sizeof(char) * (i + n + 1));
+if (p == NULL)
+return (NULL);
+for (a = 0; a < i; a++)
+{
+p[a] = s1[a];
+}
+for (b = 0; b < c; b++)
+{
+p[a++] = s2[b];
+}
+p[a++] = '\0';
+return (p);
 }
