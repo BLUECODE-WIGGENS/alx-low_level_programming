c3efb8f6d887726d823ad890954f86cb1525ff44
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-27 22:17:13 +0200

    building structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @name: Name of the dog.
  * @age: how old is the dog.
  * @owner: The master of the dog.
- * 
+ *
  * Description: struct dog is the attribute of a dog.
  */
 
@@ -19,7 +19,9 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *s);
 
 #endif
