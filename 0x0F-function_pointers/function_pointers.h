242c00ed0eab0758c0987e0cd551ec58cd886d17
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-29 20:18:33 +0200

    creation

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FUNCTION_H
+#define _FUNCTION_H
 #include <unistd.h>
 
-#include <stdlib.h>
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-#endif /*MAIN_H*/
+#endif
