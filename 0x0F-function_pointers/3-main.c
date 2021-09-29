59fd9f3b43e1c1df20846ea714537b0dc714d693
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-29 19:45:43 +0200

    creation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main- entry point
+ * @argc: argument count
+ * @argv: pointer to an array containing the parameters
+ * 
+ * Return: zero.
+ */
+
+int main(int argc, char *argv[])
+{
+int (*ptr)(int, int);
+int a, b;
+char *s;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+s = argv[2];
+
+if (*s != '+' && *s != '/' && *s != '-' && *s != '%' && *s != '*')
+{
+printf("Error\n");
+exit(99);
+}
+ptr = get_op_func(s);
+if (ptr == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+printf("%d\n", (a, b));
+return (0);
+}
