774cbc7e40bde5a23faf25ec9514321645e57ea7
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-09-30 11:30:46 +0200

    fit it

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name when called.
- * @name: A string
- * @f: A function of a pointer
+ * print_name - Calls a function to print a name
+ * @name: A pointer to a string
+ * @f: A pointer to a function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	void (*p)(char *);
 
-if (name != '\0' && f != '\0')
-ptr(name);
+	if (name == NULL || f == NULL)
+		return;
+	p = f;
+	p(name);
 }
