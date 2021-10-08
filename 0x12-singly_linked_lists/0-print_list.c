dd362eb0adbd311aa0038dc67e23f0db029adc6c
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-10-08 15:23:00 +0200

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,25 @@
 #include "lists.h"
 
 /**
- *print_list - prints all the elements of a list
- *@h: pointer to head
- *
- *Return: Number of nodes
- */
-
+  * print_list - Prints all elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of nodes
+  */
 size_t print_list(const list_t *h)
 {
-unsigned int num = 0;
-const list_t *ptr;
+	size_t count = 0;
 
-ptr = h;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-while (ptr != NULL)
-{
-if (ptr->str)
-{
-printf("[%u] %s\n", ptr->len, ptr->str);
-}
-else
-{
-printf("[%u] %s\n", 0, "(nil)");
-}
-ptr = ptr->next;
-num++;
-}
-return (num);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
