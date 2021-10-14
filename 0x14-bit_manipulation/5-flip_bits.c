9243d812248d843fc614378020baf557afa5ba1c
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-10-14 20:22:00 +0200

    binary

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,35 +1,25 @@
+#include <stdlib.h>
 #include "main.h"
 
-
 /**
- *search_bits - count number of bits to be flipped
- *@a: unsigned long integer
- *
- *Return: number of bits
- */
-
-unsigned int search_bits(unsigned long int a)
-{
-long int i = 0;
-
-
-while (a)
-{
-i += a & 1;
-a >>= 1;
-}
-return (i);
-}
-
-/**
- *flip_bits - bits need to flip from number to another
- *@n: unsigned integer
- *@m: unsigned integer
- *
- *Return: number of bit to flip
- */
-
+  * flip_bits - ...
+  * @n: ...
+  * @m: ...
+  *
+  * Return: ...
+  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-return (findbits(n ^ m));
+	unsigned long int mask = 0, count = 0;
+
+	mask = n ^ m;
+	while (mask)
+	{
+		if (mask & 1)
+			count++;
+
+		mask >>= 1;
+	}
+
+	return (count);
 }
