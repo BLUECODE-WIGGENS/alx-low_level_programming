b329d8064ad50e6489b6e48ad43d1b1467e7379d
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-10-14 15:47:46 +0200

    binary

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer 
+ * @index: index
+ *
+ * Return: the value of the bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int i;
+
+if (index >= 32)
+return (-1);
+
+i = 1 << index;
+i &= n;
+i >>= index;
+
+return (i);
+}
