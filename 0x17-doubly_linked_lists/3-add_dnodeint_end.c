787149b76729bc66cc5d8ae702c2dcc26934a41b
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-11-27 17:38:32 +0200

    fix a bug

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,15 +11,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 dlistint_t *new_node, *current_node;
 
-if (head == NULL)
-return (NULL);
-
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
 new_node->n = n;
 new_node->next = NULL;
+
 if (*head == NULL)
 {
 new_node->prev = NULL;
@@ -35,7 +33,6 @@ current_node = current_node->next;
 }
 current_node->next = new_node;
 new_node->prev = current_node;
-new_node->next = NULL;
 }
 return (new_node);
 }
