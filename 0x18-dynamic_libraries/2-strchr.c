ee67e42900d093202dc1cc26fbdaa880f2415003
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-11-29 13:07:30 +0200

    dynamically made

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: character
+ * Return: C in the string s or NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	    int i;
+
+	    for (i = 0; *(s + i); i++)
+	    {
+		if (*(s + i) == c)
+            {
+			return (s + i);
+            }
+	    }
+	    if (*(s + i) == c)
+        {
+		        return (s + i);
+        }
+	return (0);
+}
